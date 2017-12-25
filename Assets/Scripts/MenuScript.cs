88f761ab46935487ebe2d90a3cd728680c36e526
Val Isautier <val.isautier@gmail.com>
2017-12-25 19:13:00 +0400

    Fixed Build Settings + Menu now functionnal

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour {
 	[Range(0, 3)] 
 	private int _glovePosition = 0;
 
+	public GameObject ImageFader;
+
 	public float[] Positions = new float[3];
 
 	private float previousPressed;
 
+	public GameObject[] Characters = new GameObject[3];
+	private Animator[] _animators = new Animator[3];
+
 	// Use this for initialization
 	void Start () {
-		
+		SetGlovePosition ();
+
+		for (int i = 0; i < Characters.Length; i++) {
+			_animators[i] = Characters[i].GetComponent<Animator> ();
+		}
 	}
 	
 	// Update is called once per frame
@@ -30,9 +41,45 @@ public class MenuScript : MonoBehaviour {
 		}
 
 		previousPressed = h;
+
+		if (Input.GetButtonDown ("Fire1")) {
+			_animators [_glovePosition].SetTrigger ("Hit");
+			if (_glovePosition == 0 || _glovePosition == 2) {
+				StartCoroutine (FadeAndChangeScene ());
+				GetComponent<Punch> ().enabled = false;
+			}
+		}
 	}
 
 	void SetGlovePosition () {
 		transform.position = new Vector3 (transform.position.x, transform.position.y, Positions [_glovePosition]);
+	}
+
+	IEnumerator FadeAndChangeScene () {
+		Image img = ImageFader.GetComponent<Image> ();
+		Color col = img.color;
+
+		while (col.a < 1) {
+			col.a += Time.deltaTime / 2;
+			img.color = col;
+			yield return null;
+		}
+			
+		switch (_glovePosition) {
+		case 0:
+			AsyncOperation asyncLoad = SceneManager.LoadSceneAsync ("MainScene");
+			while (!asyncLoad.isDone) {
+				yield return null;
+			}
+			break;
+
+		case 2:
+			yield return null;
+			Application.Quit ();
+			break;
+
+		default:
+			break;
+		}
 	}
 }
